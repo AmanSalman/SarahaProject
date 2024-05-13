1967da02c300edefcea8a0657d737b44f24b1067
AmanSalman <82624209+AmanSalman@users.noreply.github.com>
2024-05-13 05:18:20 +0300

    Update user.router.js

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -3,7 +3,7 @@ import * as UserConrtoller from './user.controller.js';
 import { auth } from "../../middleware/authentication.js";
 
 const router = Router();
-router.get('/profile', auth , UserConrtoller.Profile);
+router.get('/profile', auth() , UserConrtoller.Profile);
 
 
 export default router;
